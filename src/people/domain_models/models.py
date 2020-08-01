921fa6a267f05cde35b464e7f29d6e8a79a2698a
Przemek <przemic98@gmail.com>
2020-08-01 16:48:04 +0200

    Add calc days until next anniversary

--- src/people/domain_models/models.py
+++ src/people/domain_models/models.py
@@ -1,13 +1,13 @@
 """ Domain models used throughout the app """
 
-from datetime import date
+import datetime
 
 
 class Person:
     """ Class to keep track of every person info """
 
     def __init__(self, gender: str, title: str, first_name: str, second_name: str,
-                 date_of_birth: date):
+                 date_of_birth: datetime.date):
         self.gender = gender
         self.title = title
         self.first_name = first_name
@@ -16,27 +16,68 @@ class Person:
 
     @property
     def age(self):
-        return self.date_of_birth.years_difference(date.today())
+        return self.date_of_birth.calculate_age()
+
+    @property
+    def days_to_birthday(self):
+        return self.date_of_birth.days_to_anniversary()
 
 
 class Date:
     """ Class to store info about dates """
 
-    def __init__(self, date_str: date):
-        self._date = date_str
+    def __init__(self, date_str: datetime.date):
+        self.date = date_str
 
-    def years_difference(self, other_date: date):
+    @property
+    def day(self):
+        return self.date.day
+
+    @property
+    def month(self):
+        return self.date.month
+
+    @property
+    def year(self):
+        return self.date.year
+
+    def calculate_age(self):
         """
-        Calculate years between two dates
-        :param other_date: Another date to count years difference
+        Calculate years from self date until today
         :return years: Years difference
         """
 
-        years = self._date.year - other_date.year - (
-                (self._date.month, self._date.day) < (other_date.month,
-                                                      other_date.day))
-        # Case when user gives elder other date than self _date
-        if years < 0:
-            years = (years * (-1)) - 1
+        today = datetime.date.today()
+
+        years = today.year - self.year - ((today.month, today.day) < (self.month,
+                                                                      self.day))
 
         return years
+
+    def days_to_anniversary(self):
+        """ Calculate days left to anniversary """
+
+        today = datetime.date.today()
+
+        if self._had_anniversary_this_year(today):
+            anniversary_year = today.year + 1
+        else:
+            anniversary_year = today.year
+
+        anniversary_date = datetime.date(anniversary_year, self.date.month,
+                                         self.date.day)
+
+        return (anniversary_date - today).days
+
+    def _had_anniversary_this_year(self, today: datetime.date):
+        """
+        Check if anniversary already happened this year
+        :param today: Today's date
+        :return: True if anniversary already happened this year
+        """
+
+        return (
+                (today.month == self.date.month and
+                 today.day > self.date.day) or
+                (today.month > self.date.month)
+        )
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,8 +1,16 @@
 from datetime import date
+from unittest import mock
+from unittest.mock import patch
 
 import pytest
 
 from src.people.domain_models.models import Person, Date
+
+
+class FakeDate(date):
+
+    def __new__(cls, *args, **kwargs):
+        return date.__new__(date, *args, **kwargs)
 
 
 class FakePerson(Person):
@@ -11,19 +19,40 @@ class FakePerson(Person):
         self.date_of_birth = Date(date_of_birth)
 
 
+@mock.patch('src.people.domain_models.models.datetime.date', FakeDate)
 @pytest.mark.parametrize('date_1, date_2, diff', [
     (date.fromisoformat('1993-07-20'), date.fromisoformat('2020-07-30'), 27),
-    (date.fromisoformat('2020-07-30'), date.fromisoformat('1993-07-20'), 27),
     (date.fromisoformat('2020-07-20'), date.fromisoformat('2020-07-30'), 0),
-    (date.fromisoformat('2020-07-30'), date.fromisoformat('2020-07-20'), 0)
 ])
 def test_years_difference(date_1, date_2, diff):
+    FakeDate.today = classmethod(lambda cls: date(2020, 7, 31))
     date_ = Date(date_1)
-    years_diff = date_.years_difference(date_2)
+    years_diff = date_.calculate_age()
     assert years_diff == diff
 
 
+@mock.patch('src.people.domain_models.models.datetime.date', FakeDate)
+@pytest.mark.parametrize('date_1, expected_days_to_anniversary', [
+    (date.fromisoformat('1993-08-01'), 1),
+    (date.fromisoformat('1993-07-31'), 0),
+    (date.fromisoformat('1993-07-30'), 364),
+])
+def test_date_anniversary_days_left(date_1, expected_days_to_anniversary):
+    FakeDate.today = classmethod(lambda cls: date(2020, 7, 31))
+    date_ = Date(date_1)
+    assert date_.days_to_anniversary() == expected_days_to_anniversary
+
+
+@mock.patch('src.people.domain_models.models.datetime.date', FakeDate)
 def test_person_age():
+    FakeDate.today = classmethod(lambda cls: date(2020, 7, 31))
     date_of_birth = date.fromisoformat('1993-07-20')
     person = FakePerson(date_of_birth=date_of_birth)
     assert person.age == 27
+
+
+@mock.patch('src.people.domain_models.models.datetime.date', FakeDate)
+def test_person_days_until_birthday():
+    FakeDate.today = classmethod(lambda cls: date(2020, 7, 31))
+    person = FakePerson(date_of_birth=date.fromisoformat('1993-08-01'))
+    assert person.days_to_birthday == 1
