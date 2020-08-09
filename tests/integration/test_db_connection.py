faf584aa9608c7b5720ac9443359f0ec36c5e4d7
Przemek <przemic98@gmail.com>
2020-08-09 14:46:07 +0200

    Update repository and db connection tests with new fixes

--- tests/integration/test_db_connection.py
+++ tests/integration/test_db_connection.py
@@ -3,25 +3,24 @@ import pytest
 from src.people.service_layer.db_connection import SqlAlchemyDbConnection
 
 
-def try_to_add_user(session, user_fixture):
-    user = user_fixture
+def try_to_add_user(session, user_factory_fixture):
+    user = user_factory_fixture()
 
     with SqlAlchemyDbConnection() as conn:
         conn.database.add(user)
 
     db_user = session.execute(
-        'SELECT * FROM user WHERE user_uuid=:user_uuid',
-        dict(user_uuid='uuid')
+        'SELECT * FROM user'
     )
 
     assert db_user == user
 
 
-def test_db_connection_rollback_on_error(session, user_fixture):
+def test_db_connection_rollback_on_error(session, user_factory_fixture):
     class MyException(Exception):
         pass
 
-    user = user_fixture
+    user = user_factory_fixture()
 
     with pytest.raises(MyException):
         with SqlAlchemyDbConnection() as conn:
